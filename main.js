5cd278c1d2c924fa66e9bb2e0dd8427547a5c8f6
yucheng514 <561890199@qq.com>
2024-05-12 02:46:24 +0800

    chore: import vue origin file

--- main.js
+++ main.js
@@ -1,24 +1,27 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+import "./style.css";
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+// import Vue from "./vue"
+import { Vue } from "./vue";
 
-setupCounter(document.querySelector('#counter'))
+
+
+var MyComponent = Vue.extend({
+    template: "<div>A custom component!</div>",
+});
+// 注册
+Vue.component("my-component", MyComponent);
+// debugger
+var app = new Vue({
+    el: "#app",
+    data: {
+        message: "Hello Vue!",
+    },
+    methods: {
+        click(event) {
+            console.log(typeof event); // object
+        },
+    },
+    created() {
+        console.log("dd");
+    },
+});
